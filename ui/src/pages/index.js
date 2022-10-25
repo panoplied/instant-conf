f345afee1f592e4cf99baa6e037a2799a9d90240
panoplied <panoplied@protonmail.com>
2022-10-25 17:42:46 +0300

    Fix preloading fonts

--- ui/src/pages/index.js
+++ ui/src/pages/index.js
@@ -7,31 +7,33 @@ import ThemeToggle from '../components/ThemeToggle';
 
 export default function Home() {
   return (
-    <div className="
-      font-robotoMonoRegular
-      min-h-screen
-      bg-stone-50
-      dark:bg-stone-900
-    ">
-
+    <>
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="instant-conf web ui" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/fonts/RobotMono-Regular.ttf" as="font" crossOrigin="" type="font/ttf" />
-        <link rel="preload" href="/fonts/RobotMono-Light.ttf" as="font" crossOrigin="" type="font/ttf" />
+        <link rel="preload" href="/fonts/RobotoMono-Regular.ttf" as="font" crossOrigin="" type="font/ttf" />
+        <link rel="preload" href="/fonts/RobotoMono-Light.ttf" as="font" crossOrigin="" type="font/ttf" />
       </Head>
 
-      <div className="flex flex-row-reverse p-4">
-        <ThemeToggle/>
-      </div>
+      <div className="
+        font-robotoMonoRegular
+        min-h-screen
+        bg-stone-50
+        dark:bg-stone-900
+      ">
 
-      <div className="grid place-items-center">
-        <ConfigContextProvider>
-          <Config />
-        </ConfigContextProvider>
-      </div>
+        <div className="flex flex-row-reverse p-4">
+          <ThemeToggle/>
+        </div>
+
+        <div className="grid place-items-center">
+          <ConfigContextProvider>
+            <Config />
+          </ConfigContextProvider>
+        </div>
 
     </div>
+  </>
   );
 }
