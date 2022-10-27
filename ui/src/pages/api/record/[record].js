6fda783c435a4c4c0139927924685dc12c0c7248
panoplied <panoplied@protonmail.com>
2022-10-27 04:14:10 +0300

    Refactor to reflect real parameter name (`key`)

--- ui/src/pages/api/record/[record].js
+++ ui/src/pages/api/record/[record].js
@@ -2,10 +2,10 @@ import { getRecord } from '../../../lib/redis';
 
 export default async function handler(req, res) {
 
-  const { record } = req.query;
+  const { record: key } = req.query;
 
   try {
-    const rec = await getRecord(record);
+    const rec = await getRecord(key);
     return res.status(200).json(rec);
   } catch (e) {
     return res.status(500).json({ error: e.message });
